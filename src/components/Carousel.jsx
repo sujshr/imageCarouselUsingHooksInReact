7e9161bfc4a2676354fe9a2678896185ad98d06d
Sujal Shresth <sujshrest@example.com>
2024-01-23 15:46:38 +0530

    Assignment Completed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+// you can explore more - and check as how to use materiul ui
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
-function Carousel() {
+function Carousel1() {
+  const [index, updateIndex] = useState(0);
+  const [title, updateTitle] = useState(images[0].title);
+  const [subtitle, updateSubTitle] = useState(images[0].subtitle);
+  const [source, updateSource] = useState(images[0].img);
+
+  useEffect(() => {
+    updateTitle(images[index].title);
+    updateSubTitle(images[index].subtitle);
+    updateSource(images[index].img);
+  }, [index]);
+
+  function handleLeftClick() {
+    index === 0
+      ? updateIndex(images.length - 1)
+      : updateIndex((prevIndex) => prevIndex - 1);
+  }
+  function handleRightClick() {
+    index === images.length - 1
+      ? updateIndex(0)
+      : updateIndex((prevIndex) => prevIndex + 1);
+  }
+  return (
+    <>
+      <div id="container">
+        <div id="left" className="arrow" onClick={handleLeftClick}>
+          <ArrowBackIosIcon />
+        </div>
+
+        <div id="image">
+          <h3 id="title">{title}</h3>
+          <div id="imgContainer">
+            <img src={source} alt="" className="img" />
+          </div>
+
+          <h5 id="subtitle">{subtitle}</h5>
+        </div>
+
+        <div id="right" className="arrow" onClick={handleRightClick}>
+          <ArrowForwardIosIcon />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Carousel;
+export default Carousel1;
